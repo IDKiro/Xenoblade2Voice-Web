2f63a3aa13059f84c0b8baf89b53064981b47151
IDKiro <609568960@qq.com>
2018-11-23 10:56:33 +0800

    init

---
+++ src/components/Player.vue
@@ -0,0 +1,26 @@
+<template>
+  <aplayer :music="playList[0]" :list="playList" repeat="none" ref="player"/>
+</template>
+
+<script>
+import Aplayer from 'vue-aplayer'
+
+export default {
+  watch: {
+    playList () {
+      this.$refs.player.thenPlay()
+    }
+  },
+  computed: {
+    playList () {
+      return this.$store.getters.playList
+    },
+    selectedPattern () {
+      return this.$store.state.selectedPattern
+    }
+  },
+  components: {
+    Aplayer
+  }
+}
+</script>
